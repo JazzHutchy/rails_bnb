9a67807c6c8e411c2c85d6aea64980aeee52b949
Jared Hutchinson <jared.hutchinson99@gmail.com>
2017-10-12 17:15:59 +1100

    Add messaging system

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,12 +1,13 @@
 class ConversationsController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
+  
  def index
   @users = User.all
   @conversations = Conversation.all
   end
+
  def create
-  if Conversation.between(params[:sender_id],params[:recipient_id])
-    .present?
+  if Conversation.between(params[:sender_id],params[:recipient_id]).present?
      @conversation = Conversation.between(params[:sender_id],
       params[:recipient_id]).first
   else
@@ -14,6 +15,7 @@ class ConversationsController < ApplicationController
   end
   redirect_to conversation_messages_path(@conversation)
  end
+
  private
   def conversation_params
    params.permit(:sender_id, :recipient_id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Listing.create!([
-  { title: 'Parliament House', street_address: 'Spring St, East Melbourne VIC 3002', city: 'Melbourne', country_code: 'au', bed_count: rand(1..100), bedroom_count: 100, bathroom_count: 100, description: 'There is a new government initiative to rent out government buildings to provide shelter for normal people.', night_fee_cents: 5000, cleaning_fee_cents: 500 },
+  { host_id: 1, title: 'Parliament House', street_address: 'Spring St, East Melbourne VIC 3002', city: 'Melbourne', country_code: 'au', bed_count: rand(1..100), bedroom_count: 100, bathroom_count: 100, description: 'There is a new government initiative to rent out government buildings to provide shelter for normal people.', night_fee_cents: 5000, cleaning_fee_cents: 50 },
   
-  { title: 'Mt Krakatoa', street_address: 'South Lampung Regency, Lampung', city: 'Some city covered in volcanic ash by now', country_code: 'pw', bed_count: 200, bedroom_count: 200, bathroom_count: 2, description: 'A really hot location tourists are staying at this summer, emphasis on hot.', night_fee_cents: 10000, cleaning_fee_cents: 1000 },
+  { host_id: 2, title: 'Mt Krakatoa', street_address: 'South Lampung Regency, Lampung', city: 'Some city covered in volcanic ash by now', country_code: 'pw', bed_count: 200, bedroom_count: 200, bathroom_count: 2, description: 'A really hot location tourists are staying at this summer, emphasis on hot.', night_fee_cents: 10000, cleaning_fee_cents: 1000 },
 
   { title: 'The Great Pyramids at Giza', street_address: 'Al Haram, Nazlet El-Semman, Al Haram, Giza Governorate, Egypt', city: 'Cairo', country_code: 'eg', bed_count: 300, bedroom_count: 300, bathroom_count: 10, description: 'Sleep with a bunch dead egyptian royal guys or something.', night_fee_cents: 4500, cleaning_fee_cents: 450 },
 ])
